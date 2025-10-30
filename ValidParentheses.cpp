543651adc1b73b55df59c578b0133d30ee624ec6
Chelsea Nissen <111597196+ChelseaDNissen@users.noreply.github.com>
2025-10-30 13:28:22 -0500

    Update ValidParentheses.cpp

--- ValidParentheses.cpp
+++ ValidParentheses.cpp
@@ -1,32 +1,36 @@
+#include <iostream>
+#include <stack>
+using namespace std; 
 class Solution {
 public:
     bool isValid(string s) {
         stack<char> mystack;
-        for(char a:s){
-            //if open paratenesis
-            if ( a== '['||a== '('||a=='{')
-                mystack.push(a);          
+          char b;
+     for (int i=0; i<s.length(); i++) {
+        if (s[i]=='('||s[i]=='['||s[i]=='{') {
+            mystack.push(s[i]);
+        
+        }else if (s[i]== ']'){
+                if (i==0||mystack.top()!='[')
+                        return false;
+                else
+                    mystack.pop();
                 
-            if (a== ']'){
-                if (mystack.top()=='[')
-                mystack.pop();
+            }else if (s[i]== '}'){
+                    if (i==0||mystack.top()!='{')
+                        return false;
                 else
-                return false;
-            }
-            if (a== '}'){
-                if (mystack.top()=='{')
-                mystack.pop();
+                    mystack.pop();
+                }else if (s[i]== ')'){
+                    if (i==0||mystack.top()!='(')
+                        return false;
                 else
-                return false;
-            }
-            if (a== ')'){
-                if (mystack.top()=='(')
-                mystack.pop();
-                else
-                    return false;
-            }
+                    mystack.pop();
+                }
 
-        }
-        return true;
     }
+        return mystack.empty();
+}
+    
 };
+
